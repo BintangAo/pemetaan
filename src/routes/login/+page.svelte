2fc34d560f108e0ee899bff17de1880c730de13a
BintangAo <valmero33@gmail.com>
2025-01-29 21:46:59 +0700

    Remove disable

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -8,7 +8,6 @@
 	import { toast } from 'svelte-sonner';
     import type {ActionData} from './$types'
     let {form} :{form:ActionData} = $props()
-    let disable = $state(false)
     $effect(()=>{
         if(form?.success){
             toast.success('Berhasil masuk, mengalihkan ke halaman user!')
@@ -21,14 +20,6 @@
         }
         if(form?.error){
             toast.error('Terjadi masalah pada server!')
-        }
-    })
-    $effect(()=>{
-        if(form?.incorrect){
-            disable = !disable
-        }
-        if(form?.error){
-            disable = !disable
         }
     })
 </script>
@@ -45,7 +36,7 @@
                 <Input type="nisn" name="nisn" id="nisn" placeholder="Masukan NISN"/>
                 <Label for="password">Password</Label>
                 <Input type="password" name="password" id="password" placeholder="Masukan Password"/>
-                <Button class="ml-auto w-16" type="submit" disabled={disable} onclick={()=>disable=!disable}>Login</Button>
+                <Button class="ml-auto w-16" type="submit">Login</Button>
             </form>
         </Card.Content>
     </Card.Root>
