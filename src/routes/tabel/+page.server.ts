d2d3bd268d03c1425f42cd25886bdf7e201d1255
BintangAo <valmero33@gmail.com>
2025-02-01 16:12:57 +0700

    Change table content

--- src/routes/tabel/+page.server.ts
+++ src/routes/tabel/+page.server.ts
@@ -1,6 +1,6 @@
 import type {PageServerLoad} from "./$types"
 import supabase from "$lib/supabase";
-import { fail } from "@sveltejs/kit";
+import { error as err } from "@sveltejs/kit";
 
 type Datas = {
     name: string;
@@ -13,14 +13,26 @@ type Datas = {
 }[]
 
 export const load: PageServerLoad = async () => {
-    const {data,error}  = await supabase.from('users').select("*").order('rank')
+    const {data,error}  = await supabase.from('users').select("first_univ,second_univ")
     if (error) {
-        throw fail(500,{message:error.message})
+        throw err(500,{message:error.message})
     }
     if (data.length===0){
-        throw fail(404,{message:"Data not found"})
+        throw err(404,{message:"Data not found"})
     }
+    const countUniv:{[key:string]:number} = {}
+    data.forEach((e)=>{
+        if(e.first_univ){
+            const first_univ = e.first_univ  as string
+            countUniv[first_univ] = (countUniv[first_univ]||0)+1
+        } 
+        if(e.second_univ){
+            const second_univ =e.second_univ as string
+            countUniv[second_univ] = (countUniv[second_univ]||0)+1
+        }
+    })
     return {
+        countUniv,
         users:data as Datas
     }
 };
--- src/routes/tabel/+page.svelte
+++ src/routes/tabel/+page.svelte
@@ -12,49 +12,26 @@
 <Button class={cn('m-12 mb-0')} href="/user">Kembali ke Halaman Pengguna</Button>
 <Card.Root class={cn('m-12')}>
 	<Card.Header>
-		<Card.Title>Tabel Pemetaan</Card.Title>
-		<Card.Description>Berikut adalah tabel pemetaan pilihan universitas dan prodi</Card.Description>
+		<Card.Title>Tabel Universitas Pilihan</Card.Title>
+		<Card.Description>Berikut adalah tabel pemetaan pilihan universitas seluruh peserta</Card.Description>
 	</Card.Header>
 	<Card.Content class="min-w-full">
 		<Table.Root class="min-w-full">
 			<Table.Header>
 				<Table.Row>
-					<Table.Head class="c w-[16px]">Ranking</Table.Head>
+					<Table.Head class="c w-[16px]">No</Table.Head>
 					<!-- <Table.Head class="text-center">Kelas</Table.Head>
 					<Table.Head class="min-w-52 text-center">Nama</Table.Head> -->
-					<Table.Head class="min-w-52 text-center">Pilihan Pertama</Table.Head>
-					<Table.Head class="min-w-52 text-center">Pilihan Kedua</Table.Head>
+					<Table.Head class="min-w-52 text-center">Universitas</Table.Head>
+					<Table.Head class="min-w-52 text-center">Jumlah</Table.Head>
 				</Table.Row>
 			</Table.Header>
 			<Table.Body>
-				{#each data.users as user}
+				{#each Object.entries(data.countUniv) as [univ,jumlah],i}
 					<Table.Row>
-						<Table.Cell class="text-center">{user.rank}</Table.Cell>
-						<!-- <Table.Cell class="text-center">{user.class}</Table.Cell>
-						<Table.Cell class="text-center">{user.name}</Table.Cell> -->
-						<Table.Cell class="text-center">
-							{#if !user.first_univ}
-								Belum dipilih
-							{:else}
-                                <span class="font-bold">
-                                    {user.first_univ}
-                                </span>
-								<Separator />
-								{user.first_prodi}
-							{/if}
-						</Table.Cell>
-
-						<Table.Cell class="p-0 text-center sm:p-4"
-							>{#if !user.second_univ}
-								Belum dipilih
-                            {:else}
-                                <span class="font-bold">
-                                    {user.second_univ} 
-                                </span>
-                                <Separator/>
-                                {user.second_prodi}
-							{/if}
-						</Table.Cell>
+						<Table.Cell class="text-center">{i+1}</Table.Cell>
+						<Table.Cell class="text-center">{univ}</Table.Cell>
+						<Table.Cell class="text-center">{jumlah}</Table.Cell>
 					</Table.Row>
 				{/each}
 			</Table.Body>
